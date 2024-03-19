8ee67ba19517f7b6a7c66e80944200d29dcfa980
Kate Howard <k.l.howard.1992@gmail.com>
2024-03-19 12:46:26 +0000

    Reorder list items

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ function App() {
   function addItem(name) {
     // Set uuid for new items so they can be uniquely identified
     const id = crypto.randomUUID();
-    setItems(existing => [...existing, { id, name, crossed: false }]);
+    setItems(existing => [
+      ...existing,
+      { id, name, crossed: false , position: existing.length}
+    ]);
   }
 
-  
   function deleteItem(id) {
     setItems(existing => existing.filter(item => item.id !== id));
   }
@@ -24,6 +26,23 @@ function App() {
     }));
   }
 
+  function moveItem(id, oldPos, newPos) {
+    setItems(existing => existing.map(item => {
+      if (item.id === id) {
+        // If dragged item, set new position
+        return { ...item, position: newPos };
+      } else if (oldPos < newPos && oldPos < item.position && item.position <= newPos) {
+        // Draged item moved up, position of item in between decreases
+        return { ...item, position: item.position - 1 };
+      } else if (newPos < oldPos && newPos <= item.position && item.position < oldPos) {
+        // Dragged item moved down, position of item in between increases
+        return { ...item, position: item.position + 1 };
+      } else {
+        return item;
+      }
+    }))
+  }
+
   // Store shopping list in localStorage whenever items updates
   useEffect(() => {
     localStorage.setItem("shopping-list", JSON.stringify(items));
@@ -31,7 +50,7 @@ function App() {
 
   return (
     <>
-      <List items={items} deleteItem={deleteItem} crossItem={crossItem} />
+      <List items={items} deleteItem={deleteItem} crossItem={crossItem} moveItem={moveItem} />
       <AddItemForm addItem={addItem} />
     </>
   );
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,39 @@
-function List({ items, deleteItem, crossItem }) {
+import { useState } from "react";
+
+function List({ items, deleteItem, crossItem, moveItem }) {
+  const [draggedItem, setDraggedItem] = useState(null);
+  const sorted = items.sort((a, b) => a.position - b.position);
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDragStart(item) {
+    setDraggedItem(item);
+  }
+
+  function handleDrop(e, position) {
+    e.preventDefault();
+    if (e.target.id !== draggedItem.id) {
+      moveItem(draggedItem.id, draggedItem.position, position);
+    }
+  }
+
   return (
-    <ul className="list">
-      {items.map(({ id, name, crossed }) => (
-        <li key={id} className="list-item">
-          <span onClick={() => crossItem(id)} className={crossed ? "crossed-item-name" : "item-name"}>
+    <ul className="list" onDragOver={handleDragOver}>
+      {sorted.map(({ id, name, crossed, position }) => (
+        <li
+          key={id}
+          id={id}
+          className="list-item"
+          draggable={true}
+          onDragStart={() => handleDragStart({ id, position })}
+          onDrop={e => handleDrop(e, position)}
+        >
+          <span
+            onClick={() => crossItem(id)}
+            className={crossed ? "crossed-item-name" : "item-name"}
+          >
             {name}
           </span>
           <button onClick={() => deleteItem(id)}>❌</button>
